8ef713976b8c41003fd6d0bc870b4c6d6e14010f
BernH4 <bernhard.steidl14@gmail.com>
2021-08-13 12:37:49 +0200

    now working with multiple input comma seperated

--- sicx_zsp_string_auskommentieren/main.rb
+++ sicx_zsp_string_auskommentieren/main.rb
@@ -1,9 +1,10 @@
 require 'fileutils'
-# require 'pry-byebug'
+require 'pry-byebug'
 
-puts 'Nach welchem String soll gesucht bzw auskommentiert werden? z.B. _221/Z'
-search_string = gets.chomp
-# search_string = '_221/Z'
+puts 'Nach welchem String soll gesucht bzw auskommentiert werden?'
+puts 'Mehrere Dateien , getrennt z.B. _221/Z,_121/Z,_353/Z'
+search_strings = gets.chomp
+# search_strings = '_221/Z,_121/Z'
 
 puts 'Welcher String soll als Kommentar angehängt werden? z.B. Migration Juni2021'
 # suffix = 'Migration Juni2021'
@@ -20,22 +21,25 @@ FileUtils.copy_entry('anzupassende_zsp', 'tmp')
 Dir.mkdir('angepasste_zsp') unless Dir.exist?('angepasste_zsp')
 
 filecounter = 0
+change_counter = 0
 Dir.glob('tmp/*') do |file|
   filecounter += 1
   basename = File.basename(file)
-  change_counter = 0
 
   file_arr = File.readlines("tmp/#{basename}",  encoding: 'iso-8859-1')
   printf("\e[1;32m%-14s\e[m %-8s %s\n", 'Bearbeite:', basename, '')
+
+  local_change_counter = 0
   new_file_arr = []
 
   file_arr.each do |line|
-    if line.include?(search_string)
+    if search_strings.split(",").any? {|search_part| line.include?(search_part) }
       new_line = line.chomp.delete('#').prepend('#') + ' ' + suffix + ' #'
       new_file_arr << new_line
 
       printf("\e[1;33m%-14s\e[m %s\n", 'Angepasst:', new_line)
       change_counter += 1
+      local_change_counter += 1
 
     # elsif line.match?(/^#/)
     #   new_file_arr << line
@@ -45,7 +49,7 @@ Dir.glob('tmp/*') do |file|
     end
   end
 
-  if change_counter > 0
+  if local_change_counter > 0
     File.open("angepasste_zsp/#{basename}", 'w+') do |file|
       file.puts(new_file_arr)
     end
@@ -54,7 +58,7 @@ Dir.glob('tmp/*') do |file|
   end
 
   # printf("\n\e[1;32m%-14s\e[m %-8s %s\n", 'Abgeschlossen:',basename , '')
-  # printf("\e[1;32m%-5s\e[m %-8s %s\n", '',"Kommentare verschoben: " , "#{change_counter}")
+  printf("\e[1;36m%-14s\e[m %s\n", 'Anzahl in ZSP', local_change_counter) unless local_change_counter == 0
   # printf("\e[1;32m%-5s\e[m %-8s %s\n", '',"Scripte gefunden: " , "#{filecounter}")
 
   # puts "Anzahl gefundener Kommentare: #{File.read("anzupassende_zsp/#{basename}").scan(/\/\*.*\*\//).count}"
@@ -63,4 +67,5 @@ end
 FileUtils.rm_rf('tmp')
 
 # system("find angepasste_zsp/ -type f | xargs unix2dos -q")
+puts "Änderungen insgesamt: " + change_counter.to_s
 puts 'Im Ordner "angepasste_zsp" befinden sich nun nur noch Scripte in denen es Änderungen gab.'
