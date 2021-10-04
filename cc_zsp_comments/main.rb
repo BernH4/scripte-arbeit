3c2e6e2636bb5cf74b711cff0f2f2924d7a00d3b
Bernhard S <bernhard.s.dev@gmail.com>
2021-10-04 16:14:53 +0200

    moved convert newline to new bash file because of missinterpretation by ruby

--- cc_zsp_comments/main.rb
+++ cc_zsp_comments/main.rb
@@ -9,7 +9,7 @@ FileUtils.rm_rf('tmp')
 Dir.mkdir('tmp')
 FileUtils.copy_entry('anzupassende_scripte', 'tmp')
 system('dos2unix -q tmp/*')
-system("sed -i 's++\n+' tmp/*")
+system('bash convert_newline.sh')
 
 filecounter = 0
 Dir.glob('anzupassende_scripte/*') do |file|
